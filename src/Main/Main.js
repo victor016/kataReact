74f44af8ea75ede07e6b9206e06847c6c494b777
Juan Pablo Balda <jpbalda@hotmail.com>
2019-08-30 17:28:01 -0500

    Deleted content in main component

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -9,18 +9,6 @@ class Main extends Component {
   render() {
     return (
       <div className="container">
-        <p className="App-intro">
-          El ejercicio consiste en recorrer un array en espiral de izquierda a derecha, 
-          se deben realizar test de lo que se crea necesario del código con JEST.
-          Para poder realizar el ejercicio es necesario:
-        </p>
-        
-        <ol>
-          <li>Crear e imprimir el array original de dos dimensiones nxn</li>
-          <li>Imprimir el output requerido</li>
-        </ol>
-        
-        <img className="example" src="/assets/example.png"/>
       </div>
     );
   }
