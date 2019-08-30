3b735d6a4883aaea03f57dde0660d91fe6ede0a0
Franklin <frankpaul142@gmail.com>
2019-08-30 18:09:32 -0500

    added circleci

---
+++ ci/deployment.sh
@@ -0,0 +1,25 @@
+# !/bin/bash
+
+set -e
+
+echo "Deploying"
+
+echo $ACCOUNT_KEY > ${HOME}/gcloud-service-key.json
+
+gcloud auth activate-service-account ${ACCOUNT_ID} --key-file ${HOME}/gcloud-service-key.json
+
+gcloud config set project coding-dojo-251421
+
+gcloud --quiet config set container/cluster coding-dojo
+
+gcloud config set compute/zone us-central1-b
+
+gcloud --quiet container clusters get-credentials coding-dojo
+
+docker build -t gcr.io/coding-dojo-251421/coding-dojo:$CIRCLE_SHA1 .
+
+gcloud docker -- push gcr.io/${PROJECT_ID}/coding-dojo:$CIRCLE_SHA1
+
+kubectl set image deployment/coding-dojo coding-dojo=gcr.io/coding-dojo-251421/coding-dojo:$CIRCLE_SHA1
+
+echo " Successfully deployed"
