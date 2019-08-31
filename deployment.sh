d4f2d346184e80240433c6d32cfe7d881bf6a28a
Victor <victor.salazar593@gmail.com>
2019-08-31 14:21:59 -0500

    fix deployment

--- deployment.sh
+++ deployment.sh
@@ -14,12 +14,12 @@ gcloud --quiet config set container/cluster coding-dojo-victors
 
 gcloud config set compute/zone us-central1-a
 
-gcloud --quiet container clusters get-credentials coding-dojo
+gcloud --quiet container clusters get-credentials coding-dojo-victors
 
 docker build -t gcr.io/coding-dojo-251421/coding-dojo:$CIRCLE_SHA1 .
 
 gcloud docker -- push gcr.io/coding-dojo-251421/coding-dojo:$CIRCLE_SHA1
 
-kubectl set image deployment/coding-dojo-victors coding-dojo=gcr.io/coding-dojo-251421/coding-dojo:$CIRCLE_SHA1
+kubectl set image deployment/coding-dojo coding-dojo=gcr.io/coding-dojo-251421/coding-dojo:$CIRCLE_SHA1
 
 echo " Successfully deployed"
