de1797d062e9ec0e621241b7733840684bbe7121
Victor <victor.salazar593@gmail.com>
2019-08-31 16:56:03 -0500

    ejercicio 1.1

--- src/Exercise1/Exercise1.js
+++ src/Exercise1/Exercise1.js
@@ -6,10 +6,27 @@ export class Exercise1 extends Component {
     this.state = {};
   }
   
+  rotarline(alphabet, spaces) {
+    //recorre letras
+    const new_line = alphabet.substring(0, spaces) + alphabet.substring(spaces, alphabet.length);
+    return  new_line
+  }
+
+  printline() {
+    //imprime las lineas del alfabeto 
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const alphabetArray =  Array.from(alphabet)
+   return alphabetArray.map((letter, index) => {
+        const line = this.rotarline(alphabet, index + 1);
+        return <div key={letter + index}>{line}</div>;
+    });
+  }
+  
   render() {
     return (
       <div className="container">
         Exercise1 page
+        {this.printline()}
       </div>
     );
   }
